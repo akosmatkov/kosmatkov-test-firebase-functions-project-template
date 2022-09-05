69c53cf60ebddbbeaf72fac9f69cf648b767a946
Artur Gubaidullin <lapteuh@gmail.com>
2022-09-05 19:26:54 +0400

    feat: add sample test

---
+++ functions/src/index.test.js
@@ -0,0 +1,8 @@
+import {describe, it} from "mocha";
+import {assert} from "chai";
+
+describe("Cookie monster wants tests.", function () {
+  it("should be a test", function () {
+    assert.fail("Unimplemented.");
+  });
+});
